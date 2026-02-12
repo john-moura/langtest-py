a3ae4b2de4e754bb4a82b02481ca99079112d96e
John Moura <johnmoura.88@gmail.com>
2026-02-12 21:41:10 +0100

    Models

--- src/course/models.py
+++ src/course/models.py
@@ -10,17 +10,29 @@ class Course(db.Model):
     subjects = db.relationship('Subject', backref = 'course', lazy = True)
 
     def __repr__(self):
-        return f"Course('{self.id}', '{self.name}', '{self.description}', '{self.created_at}', '{self.thumb_url}')"
+        return f"Course('{self.id}', 
+                '{self.name}', 
+                '{self.description}', 
+                '{self.created_at}', 
+                '{self.thumb_url}')"
 
 
 class Subject(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String)
-    description = db.Column(db.String)
+    name_en = db.Column(db.String)
+    name_de = db.Column(db.String)
+    description_en = db.Column(db.String)
+    description_de = db.Column(db.String)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     thumb_url = db.Column(db.String)
     course = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
     tests = db.relationship('Test', backref = 'subject', lazy = True)
 
     def __repr__(self):
-        return f"Subject('{self.id}', '{self.name}', '{self.description}', '{self.created_at}', '{self.thumb_url}')"
+        return f"Subject('{self.id}', 
+                '{self.name_en}', 
+                '{self.name_de}', 
+                '{self.description_en}', 
+                '{self.description_de}', 
+                '{self.created_at}', 
+                '{self.thumb_url}')"
--- src/test/models.py
+++ src/test/models.py
@@ -3,9 +3,12 @@ from src import db
 
 class Test(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String)
-    short_description = db.Column(db.String)
-    description = db.Column(db.String)
+    name_en = db.Column(db.String)
+    name_de = db.Column(db.String)
+    short_description_en = db.Column(db.String)
+    short_description_de = db.Column(db.String)
+    description_en = db.Column(db.String)
+    description_de = db.Column(db.String)
     weight = db.Column(db.numeric)
     duration = db.Column(db.Integer)
     cover_image_url = db.Column(db.String)
@@ -15,9 +18,12 @@ class Test(db.Model):
 
     def __repr__(self):
         return f"Test('{self.id}', 
-                '{self.name}', 
-                '{self.short_description}', 
-                '{self.description}', 
+                '{self.name_en}', 
+                '{self.name_de}', 
+                '{self.short_description_en}', 
+                '{self.short_description_de}', 
+                '{self.description_en}', 
+                '{self.description_de}', 
                 '{self.created_at}', 
                 '{self.weight}', 
                 '{self.duration}', 
@@ -25,12 +31,80 @@ class Test(db.Model):
                 '{self.subject}', 
                 '{self.course}')"
 
+
+class Test_Part_Type(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    parts = db.relationship('Test_Part', backref = 'part_type', lazy = True)
+
+    def __repr__(self):
+        return f"Test_Part_Type('{self.id}', 
+                '{self.name}', 
+                '{self.created_at}')"
+
+
 class Test_Part(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String)
+    title_en = db.Column(db.String)
+    title_de = db.Column(db.String)
     part_type = db.Column(db.Integer, db.ForeignKey('part_type.id'), nullable=True)
     test = db.Column(db.Integer, db.ForeignKey('test.id'), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    questions = db.relationship('Question', backref = 'test_part', lazy = True)
 
     def __repr__(self):
-        return f"Test_Part('{self.id}', '{self.title}', '{self.part_type}', '{self.test}', '{self.created_at}')"
+        return f"Test_Part('{self.id}', 
+                '{self.title_en}', 
+                '{self.title_de}', 
+                '{self.part_type}', 
+                '{self.test}', 
+                '{self.created_at}', 
+                '{self.questions}')"
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    idx = db.Column(db.Integer)
+    header_en = db.Column(db.String)
+    header_de = db.Column(db.String)
+    subheader_en = db.Column(db.String)
+    subheader_de = db.Column(db.String)
+    text_en = db.Column(db.String)
+    text_de = db.Column(db.String)
+    image_url = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    test_part = db.Column(db.Integer, db.ForeignKey('test_part.id'), nullable=True)
+    answers = db.relationship('Answer', backref = 'question', lazy = True)
+    
+    def __repr__(self):
+        return f"Question('{self.id}', 
+                '{self.idx}', 
+                '{self.header_en}', 
+                '{self.header_de}', 
+                '{self.subheader_en}', 
+                '{self.subheader_de}', 
+                '{self.text_en}', 
+                '{self.text_de}', 
+                '{self.image_url}', 
+                '{self.created_at}', 
+                '{self.test_part}')"
+
+
+class Answer(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    idx = db.Column(db.String)
+    text_en = db.Column(db.String)
+    text_de = db.Column(db.String)
+    is_correct = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    question = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=True)
+
+    def __repr__(self):
+        return f"Answer('{self.id}', 
+                '{self.idx}', 
+                '{self.text_en}', 
+                '{self.text_de}', 
+                '{self.is_correct}', 
+                '{self.created_at}', 
+                '{self.question}')"
